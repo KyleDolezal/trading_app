bc0d63fae6b26da61172e36541361fc314b4e5ff
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2025-02-11 17:38:38 -0600

    Load environment variables from env file

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+from dotenv import load_dotenv
 from handle_exit import handle_exit
 import logging
 logger = logging.getLogger(__name__)
@@ -5,6 +6,8 @@ logger = logging.getLogger(__name__)
 def main():
     logging.basicConfig(filename='logs/app.log', level=logging.INFO, format='%(asctime)s %(levelname)-8s %(message)s', datefmt='%Y-%m-%d %H:%M:%S')
     print("Welcome to the trading app. Hit \'q\' to quit.")
+
+    load_dotenv()
 
     while True:
         handle_exit()
