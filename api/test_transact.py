aed32e7e9533b53134242940e72158c54717c4a5
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2025-03-09 22:11:31 -0500

    Add client for buying and selling

---
+++ api/test_transact.py
@@ -0,0 +1,75 @@
+import pytest
+from transact import TransactClient
+import os
+
+
+
+class MockClient(object):
+    def __init__(self):
+        self.response = MockResponse()
+
+    def order_place(self, params, args):
+        assert params == "1234"
+        return self.response
+
+class MockResponse(object):
+    def __init__(self):
+        self.headers = {}
+        self.status_code = 201
+
+class MockClientFailed(object):
+    def __init__(self):
+        self.response = MockFailedResponse()
+
+    def order_place(self, params, args):
+        assert params == "1234"
+        return self.response
+    
+class MockFailedResponse(object):
+    def __init__(self):
+        self.headers = {}
+        self.status_code = 400
+
+def test_buy(mocker):
+    mocker.patch('transact.TransactClient.__init__', return_value=None)
+    transact_client = TransactClient()
+    transact_client.client = MockClient()
+    transact_client.target_symbol = "SCHB"
+    transact_client.account_number = "1234"
+
+
+    assert transact_client.buy(1) == transact_client.client.response
+
+def test_sell(mocker):
+    mocker.patch('transact.TransactClient.__init__', return_value=None)
+    transact_client = TransactClient()
+    transact_client.client = MockClient()
+    transact_client.target_symbol = "SCHB"
+    transact_client.account_number = "1234"
+
+    assert transact_client.sell(1, 'LIMIT', 10.0) == transact_client.client.response
+
+
+def test_buy_bad_response(mocker):
+    mocker.patch('transact.TransactClient.__init__', return_value=None)
+    transact_client = TransactClient()
+    transact_client.client = MockClientFailed()
+    transact_client.target_symbol = "SCHB"
+    transact_client.account_number = "1234"
+
+    try:
+        transact_client.buy(1) == transact_client.client.response
+    except:
+        assert True
+
+def test_sell_bad_response(mocker):
+    mocker.patch('transact.TransactClient.__init__', return_value=None)
+    transact_client = TransactClient()
+    transact_client.client = MockClientFailed()
+    transact_client.target_symbol = "SCHB"
+    transact_client.account_number = "1234"
+
+    try:
+        transact_client.sell(1, 'LIMIT', 10.0)  == transact_client.client.response
+    except:
+        assert True
---
+++ api/transact.py
@@ -0,0 +1,58 @@
+import logging
+logger = logging.getLogger(__name__)
+from api_base import ApiBase
+
+class TransactClient(ApiBase):
+    def __init__(self):
+        super().__init__()
+
+    def buy(self, quantity):
+        request_body_json = {
+            "orderType": "MARKET",
+            "session": "NORMAL",
+            "duration": "DAY",
+            "orderStrategyType": "SINGLE",
+            "orderLegCollection": [
+                {"instruction": "BUY",
+                "quantity": quantity,
+                "instrument": {
+                    "symbol": self.target_symbol,
+                    "assetType": "EQUITY"
+                }
+                }
+            ]
+        }
+
+        order = self.client.order_place(self.account_number, request_body_json)
+
+        if order.status_code != 201:
+            logger.error("Error with order: {}".format(order.headers))
+            raise Exception()
+        
+        return order
+
+    def sell(self, quantity, mode, bounds_value):
+        request_body_json = {
+            "orderType": mode,
+            "session": "NORMAL",
+            "duration": "DAY",
+            "orderStrategyType": "SINGLE",
+            "price": bounds_value,
+            "orderLegCollection": [
+                {"instruction": "SELL",
+                "quantity": quantity,
+                "instrument": {
+                    "symbol": self.target_symbol,
+                    "assetType": "EQUITY"
+                }
+                }
+            ]
+        }
+
+        order = self.client.order_place(self.account_number, request_body_json)
+
+        if order.status_code != 201:
+            logger.error("Error with order: {}".format(order.headers))
+            raise Exception()
+        
+        return order
