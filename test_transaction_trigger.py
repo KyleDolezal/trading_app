88064b636bdfec119cd5043f4344cbe7dc0446d4
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2025-03-11 19:43:10 -0500

    Add module to determine buy/sell behavior

---
+++ test_transaction_trigger.py
@@ -0,0 +1,51 @@
+import pytest
+from transaction_trigger import TransactionTrigger
+import os
+
+response = {"last":{"conditions":[1],"exchange":1,"price":83712.2,"size":0.00473092,""
+"timestamp":1741532522429},"request_id":"44ac62cbfdae6adc14158dac0d57ba1a","status":"success",
+"symbol":"BTC-USD"}
+
+class MockResponse(object):
+    def json(param):
+        return response
+    
+def test_get_crypto_quote_sell(mocker):
+    mock_account_status = mocker.patch('currency_quote.requests.get', return_value=MockResponse())
+    mock_account_status = mocker.patch('transaction_trigger.time.sleep')
+    os.environ["HISTORY_LENGTH"] = '3'
+    os.environ["CHANGE_THRESHOLD"] = '.1'
+    os.environ["CURRENCY_TICKER"] = '123'
+    os.environ["CURRENCY_API_KEY"] = 'key'
+    
+    tt = TransactionTrigger()
+    tt.get_action(10)
+    tt.get_action(10)
+    tt.get_action(13)
+    assert tt.get_action(9) == 'sell'
+
+def test_get_crypto_quote_buy(mocker):
+    mock_account_status = mocker.patch('currency_quote.requests.get', return_value=MockResponse())
+    mock_account_status = mocker.patch('transaction_trigger.time.sleep')
+    os.environ["HISTORY_LENGTH"] = '3'
+    os.environ["CHANGE_THRESHOLD"] = '.1'
+    os.environ["CURRENCY_TICKER"] = '123'
+    os.environ["CURRENCY_API_KEY"] = 'key'
+    
+    tt = TransactionTrigger()
+    tt.get_action(10)
+    tt.get_action(10)
+    assert tt.get_action(10.1) == 'buy'
+
+def test_get_crypto_quote_hold(mocker):
+    mock_account_status = mocker.patch('currency_quote.requests.get', return_value=MockResponse())
+    mock_account_status = mocker.patch('transaction_trigger.time.sleep')
+    os.environ["HISTORY_LENGTH"] = '3'
+    os.environ["CHANGE_THRESHOLD"] = '.1'
+    os.environ["CURRENCY_TICKER"] = '123'
+    os.environ["CURRENCY_API_KEY"] = 'key'
+    
+    tt = TransactionTrigger()
+    tt.get_action(10)
+    tt.get_action(10)
+    assert tt.get_action(10.01) == 'hold'
---
+++ transaction_trigger.py
@@ -0,0 +1,46 @@
+import logging
+logger = logging.getLogger(__name__)
+import os
+import statistics
+from api.currency_quote import CurrencyClient
+import time
+
+class TransactionTrigger:
+    def __init__(self):
+        self.history_length = int(os.getenv('HISTORY_LENGTH'))
+        self.change_threshold = float(os.getenv('CHANGE_THRESHOLD'))
+        self.history = []
+        self.next_action = None
+        self._boot_strap()
+    
+    def get_action(self, price):
+        self.history.append(price)
+        if len(self.history) > self.history_length:
+            self.history = self.history[1:]
+
+        average = statistics.mean(self.history)
+
+        difference = price - average
+
+        percent_difference = (difference/average) * 100
+
+        print(self.next_action, percent_difference)
+        if (self.next_action == 'buy') and (percent_difference > self.change_threshold):
+            self.next_action = 'sell'
+            return 'buy'
+        elif (self.next_action == 'sell') and (percent_difference < self.change_threshold) and abs(percent_difference) > self.change_threshold:
+            self.next_action = 'buy'
+            return 'sell'
+        else:
+            return 'hold'
+        
+    def _boot_strap(self):
+        currency_client = CurrencyClient()
+
+        for i in range(self.history_length):
+            self.history.append(currency_client.get_crypto_quote())
+            time.sleep(1)
+
+        self.next_action = 'buy'
+
+        
