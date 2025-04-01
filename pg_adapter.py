84c359eca9ebff0bb086fc283cb29ec5958b5501
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2025-04-01 18:42:07 -0500

    Autocommit db changes

--- pg_adapter.py
+++ pg_adapter.py
@@ -4,7 +4,7 @@ import os
 class PG_Adapter:
     def __init__(self):
         self.connection = psycopg2.connect(database=os.getenv('PYWAY_DATABASE_NAME'), user=os.getenv('PYWAY_DATABASE_USERNAME'), password=os.getenv('PYWAY_DATABASE_PASSWORD'), host="localhost", port=5432)
-
+        self.connection.autocommit = True
         self.cursor = self.connection.cursor()
 
     def exec_query(self, query):
