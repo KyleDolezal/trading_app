2a9af172b9cc222789020f804ca17b034ec88393
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2025-04-06 21:25:07 -0500

    Create transaction base

---
+++ transaction_base.py
@@ -0,0 +1,62 @@
+import logging
+logger = logging.getLogger(__name__)
+import os
+import statistics
+from api.currency_quote import CurrencyClient
+import time
+import datetime
+
+class TransactionBase:
+    def __init__(self):
+        self.history_length = int(os.getenv('HISTORY_LENGTH'))
+        self.change_threshold = float(os.getenv('CHANGE_THRESHOLD'))
+        self.history = []
+        self.next_action = None
+        self.currency_client = CurrencyClient()
+        self._boot_strap()
+        self.bought_price = None
+        self.today830am = datetime.datetime.now().replace(hour=8, minute=30, second=0, microsecond=0)
+        self.today230pm = datetime.datetime.now().replace(hour=14, minute=30, second=0, microsecond=0)
+    
+    def get_action(self, price):
+        self.history.append(price)
+        if len(self.history) > self.history_length:
+            self.history = self.history[1:]
+
+        percent_difference = self._get_price_difference(price)
+
+        if datetime.datetime.now() < self.today830am:
+            return 'hold'
+
+        if (self.next_action == 'buy') and \
+                (percent_difference > self.change_threshold) and \
+                (datetime.datetime.now() < self.today230pm) and \
+                not self._is_down_market():
+            self.next_action = 'sell'
+            self.bought_price = price
+            return 'buy'
+        elif (self.next_action == 'sell') and (price >= self.bought_price) \
+                and (percent_difference < self.change_threshold) \
+                and abs(percent_difference) > self.change_threshold:
+            self.next_action = 'buy'
+            return 'sell'
+        else:
+            return 'hold'
+        
+    def _get_price_difference(self, price):
+        average = statistics.mean(self.history)
+        difference = price - average
+        return ((difference/average) * 100)
+        
+    def _boot_strap(self):
+        for i in range(self.history_length):
+            self.history.append(self.currency_client.get_crypto_quote())
+            time.sleep(1)
+
+        self.next_action = 'buy'
+    
+    def _is_down_market(self):
+        return self.currency_client.get_snapshot() <= -1
+        
+    def _is_up_market(self):
+        return self.currency_client.get_snapshot() >= 1
--- transaction_trigger.py
+++ transaction_trigger.py
@@ -4,19 +4,12 @@ import os
 import statistics
 from api.currency_quote import CurrencyClient
 import time
+from transaction_base import TransactionBase
 import datetime
 
-class TransactionTrigger:
+class TransactionTrigger(TransactionBase):
     def __init__(self):
-        self.history_length = int(os.getenv('HISTORY_LENGTH'))
-        self.change_threshold = float(os.getenv('CHANGE_THRESHOLD'))
-        self.history = []
-        self.next_action = None
-        self.currency_client = CurrencyClient()
-        self._boot_strap()
-        self.bought_price = None
-        self.today830am = datetime.datetime.now().replace(hour=8, minute=30, second=0, microsecond=0)
-        self.today230pm = datetime.datetime.now().replace(hour=14, minute=30, second=0, microsecond=0)
+        super().__init__()
     
     def get_action(self, price):
         self.history.append(price)
@@ -42,21 +35,3 @@ class TransactionTrigger:
             return 'sell'
         else:
             return 'hold'
-        
-    def _get_price_difference(self, price):
-        average = statistics.mean(self.history)
-        difference = price - average
-        return ((difference/average) * 100)
-        
-    def _boot_strap(self):
-        for i in range(self.history_length):
-            self.history.append(self.currency_client.get_crypto_quote())
-            time.sleep(1)
-
-        self.next_action = 'buy'
-    
-    def _is_down_market(self):
-        return self.currency_client.get_snapshot() <= -1
-        
-    def _is_up_market(self):
-        return self.currency_client.get_snapshot() >= 1
