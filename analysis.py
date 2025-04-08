af21665a4d39fb70f81f5f6ef2fa96e9655c69d8
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2025-04-07 20:05:59 -0500

    Update analysis

--- analysis.py
+++ analysis.py
@@ -7,8 +7,15 @@ import os
 
 load_dotenv()
 
+TARGET_SYMBOL = 'IBIT'
+SOURCE_SYMBOL = 'BTC'
+START_TIMESTAMP = 1743773400000000000
+END_TIMESTAMP = 1743796800000000000
+HISTORY_LENGTH=50
+CHANGE_THRESHOLD=.01
+
 currency_client = CurrencyClient()
-equity_client = EquityClient()
+equity_client = EquityClient(TARGET_SYMBOL)
 transaction_trigger = TransactionTrigger()
 
 total = 0
@@ -16,31 +23,27 @@ sold_price = 0
 last_action = None
 bought_price = 0
 
-TARGET_SYMBOL = 'BITU'
-SOURCE_SYMBOL = 'BTC'
-START_TIMESTAMP = 1742477400000000000
-END_TIMESTAMP = 1742500800000000000
-HISTORY_LENGTH=50
-CHANGE_THRESHOLD=.01
 transaction_trigger.history_length = HISTORY_LENGTH
 transaction_trigger.change_threshold = CHANGE_THRESHOLD
 
 for timestamp in range(START_TIMESTAMP, END_TIMESTAMP, 1000000000):
-
-    resp = requests.get("https://api.polygon.io/v3/trades/X:{}-USD?timestamp.gte={}&order=asc&limit=1&sort=timestamp&apiKey={}".format(SOURCE_SYMBOL, timestamp,  os.getenv('EQUITY_API_KEY')))
-    price = resp.json()['results'][0]['price']
-    action = transaction_trigger.get_action(price)
-
-    if action != 'hold':
-        resp = requests.get("https://api.polygon.io/v3/trades/{}?timestamp.gte={}&order=asc&limit=1&sort=timestamp&apiKey={}".format(TARGET_SYMBOL, timestamp,  os.getenv('EQUITY_API_KEY')))
+    try:
+        resp = requests.get("https://api.polygon.io/v3/trades/X:{}-USD?timestamp.gte={}&order=asc&limit=1&sort=timestamp&apiKey={}".format(SOURCE_SYMBOL, timestamp,  os.getenv('EQUITY_API_KEY')))
         price = resp.json()['results'][0]['price']
-        if action == 'buy':
-            total -= price
-            last_action = 'bought'
-            bought_price = price
-        else:
-            total += price
-            last_action = 'sold'
+        action = transaction_trigger.get_action(price)
+
+        if action != 'hold':
+            resp = requests.get("https://api.polygon.io/v3/trades/{}?timestamp.gte={}&order=asc&limit=1&sort=timestamp&apiKey={}".format(TARGET_SYMBOL, timestamp,  os.getenv('EQUITY_API_KEY')))
+            price = resp.json()['results'][0]['price']
+            if action == 'buy':
+                total -= price
+                last_action = 'bought'
+                bought_price = price
+            else:
+                total += price
+                last_action = 'sold'
+    except:
+        pass
 
 if last_action == 'bought':
     total += bought_price
