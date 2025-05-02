26edb64415972406eb9786b672d140961af3897c
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2025-05-01 21:41:15 -0500

    Improve analysis with more options

--- analysis.py
+++ analysis.py
@@ -12,11 +12,13 @@ SOURCE_SYMBOL = 'BTC'
 
 START_TIMESTAMP = 1745415000000000000
 END_TIMESTAMP = 1745438400000000000
-TWO_PM = END_TIMESTAMP - 3600000000000
+TWO_THIRTY_PM = END_TIMESTAMP - 1800000000000
 
 HISTORY_LENGTH=50
-CHANGE_THRESHOLD=.05
-HOLDS_PER_OVERRIDE_CENT=7
+CHANGE_THRESHOLD=.075
+HOLDS_PER_OVERRIDE_CENT=.1
+QUICK_SELLOFF_MULTIPLIER=10
+TEST_PRESERVE_ASSET_VALUE=False
 
 currency_client = CurrencyClient()
 equity_client = EquityClient(TARGET_SYMBOL)
@@ -32,6 +34,8 @@ num_of_transactions = 0
 transaction_trigger.history_length = HISTORY_LENGTH
 transaction_trigger.change_threshold = CHANGE_THRESHOLD
 transaction_trigger.holds_per_override_cent = HOLDS_PER_OVERRIDE_CENT
+transaction_trigger.quick_selloff_multiplier = QUICK_SELLOFF_MULTIPLIER
+transaction_trigger.test_preserve_asset_value = TEST_PRESERVE_ASSET_VALUE
 for timestamp in range(START_TIMESTAMP, END_TIMESTAMP, 500000000):
     try:
         resp = requests.get("https://api.polygon.io/v3/trades/X:{}-USD?timestamp.gte={}&order=asc&limit=1&sort=timestamp&apiKey={}".format(SOURCE_SYMBOL, timestamp,  os.getenv('EQUITY_API_KEY')))
@@ -42,12 +46,14 @@ for timestamp in range(START_TIMESTAMP, END_TIMESTAMP, 500000000):
             resp = requests.get("https://api.polygon.io/v3/trades/{}?timestamp.gte={}&order=asc&limit=1&sort=timestamp&apiKey={}".format(TARGET_SYMBOL, timestamp,  os.getenv('EQUITY_API_KEY')))
             price = resp.json()['results'][0]['price']
             if action == 'buy':
+                print("bought at:", timestamp)
                 total -= price
                 last_action = 'bought'
                 bought_price = price
             else:
-                if timestamp > TWO_PM:
-                    sold_in_last_hour = True
+                print("sold at:", timestamp)
+                if timestamp > TWO_THIRTY_PM:
+                    sold_in_last_half_hour = True
                 total += price
                 last_action = 'sold'
     except:
@@ -57,5 +63,5 @@ if last_action == 'bought':
     total += bought_price
 
 print("total:", total)
-print("sold_in_last_hour:", sold_in_last_hour)
+print("sold_in_last_hour:", sold_in_last_half_hour)
 print("num of transactions:", num_of_transactions)
