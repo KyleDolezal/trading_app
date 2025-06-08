f31e4e2b155225770cf52e40028a745f15839685
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2025-06-07 20:28:18 -0500

    Use database connection pool for pg adapter

--- pg_adapter.py
+++ pg_adapter.py
@@ -1,11 +1,15 @@
 import psycopg2
+import psycopg2.pool
 import os
 import logging
 logger = logging.getLogger(__name__)
 
 class PG_Adapter:
     def __init__(self):
-        self.connection = psycopg2.connect(database=os.getenv('PYWAY_DATABASE_NAME'), user=os.getenv('PYWAY_DATABASE_USERNAME'), password=os.getenv('PYWAY_DATABASE_PASSWORD'), host="localhost", port=5432)
+        pool = psycopg2.pool.SimpleConnectionPool(
+                2, 3, user=os.getenv('PYWAY_DATABASE_USERNAME'), password=os.getenv('PYWAY_DATABASE_PASSWORD'),
+                host='localhost', port='5432', database=os.getenv('PYWAY_DATABASE_NAME'))
+        self.connection = pool.getconn()
         self.connection.autocommit = True
         self.cursor = self.connection.cursor()
 
