a6818016571c8b75b46dea15b06fa26b1e922149
Kyle Dolezal <kyle.d.dolezal@gmail.com>
2025-07-25 21:33:33 -0500

    Update configurations in transact module

--- api/transact.py
+++ api/transact.py
@@ -11,7 +11,7 @@ class TransactClient(ApiBase):
         super().__init__()
         self.target_symbol = target_symbol
         self.order_status = OrderStatus()
-        self.stop_value = float(os.getenv('STOP_VALUE', .125))
+        self.stop_value = float(os.getenv('STOP_VALUE', .2))
     
     def _transact(self, json):
         order = self.client.order_place(self.account_number, json)
@@ -26,7 +26,7 @@ class TransactClient(ApiBase):
     def buy(self, quantity, bounds_value):
         buy_price = round(bounds_value, 2)
         sell_price = round(buy_price + .01, 2)
-        stop_price = round(sell_price - .13, 2)
+        stop_price = round(sell_price - .20, 2)
 
         request_body_json = {
             "orderStrategyType": "TRIGGER",
